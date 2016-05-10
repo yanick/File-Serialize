2429ecfba2cc04147bfd3ded8bd30014ea80d691
Yanick Champoux <yanick@cpan.org>
2016-05-09 21:42:30 -0400

    JSONY

---
+++ lib/File/Serialize/Serializer/JSONY.pm
@@ -0,0 +1,42 @@
+package File::Serialize::Serializer::JSONY;
+# abstract: JSONY serializer for File::Serialize
+
+use strict;
+use warnings;
+
+use File::Serialize;
+
+use Moo;
+with 'File::Serialize::Serializer';
+
+sub extensions { qw/ jsony / };
+
+sub serialize {
+    my( $self, $data, $options ) = @_;
+    local *STDOUT;
+    open STDOUT,  '>', \my $output;
+    serialize_file '-', $data, { format => 'json' };
+    return $output;
+}
+
+
+sub deserialize {
+    my( $self, $data, $options ) = @_;
+    JSONY->new->load($data);
+}
+
+1;
+
+__END__
+
+=head1 DESCRIPTION
+
+Serializer for L<JSONY>.
+
+Registered against the extension C<jsony>.
+
+This serializer actually only deserializes. Its serialization
+is taken care of by any available JSON serializer.
+
+=cut
+
